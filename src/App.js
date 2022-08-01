a7b0175b6a0445df08f8594d55dad2be50b8e54b
Helio Junior <heliojr.zpt@gmail.com>
2022-08-01 17:20:12 -0300

    Trabalho criando um contador em react

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends Component{
+    state = {
+        numero: 0
+    };
+
+    aumentar = () => {
+        if (this.state.numero < 10) {
+                this.setState({
+                    numero: this.state.numero + 1
+                });
+            }
+
+    }
+    diminuir = () => {
+        if (this.state.numero > 0){
+                this.setState({
+                    numero: this.state.numero - 1
+                });
+        }
+    }
+        
+
+    render() {
+        return (
+            <div>
+                <h1>Trabalho Contador</h1>
+                <h2>{this.state.numero}</h2>
+                <button onClick={this.aumentar}>+</button>
+                <button onClick={this.diminuir}>-</button>
+            </div>
+        );
+    }
 }
-
-export default App;
